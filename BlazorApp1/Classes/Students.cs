98d05b0a3441078b03d2490808b8967e7b3911b8
rightec <morani.rightec@gmail.com>
2021-01-12 12:10:25 +0100

    Front-end + navigation menù

---
+++ BlazorApp1/Classes/Students.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorApp1.Classes
+{
+    public class Students
+    {
+        public string CourseName { get; set; }
+        public string GivenName { get; set; }
+        public string FamilyName { get; set; }
+    }
+}
